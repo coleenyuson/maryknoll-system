4352a052f388dfc53dcd0d9241a063c1ccc2e0b7
Aji <jwpbanawan@addu.edu.ph>
2018-02-01 01:14:33 +0000

    CHOINNA DA BEST FRONT ENDDDDUUU

--- enrollment/views.py
+++ enrollment/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def sectionList(request):
-    
-    return render(request,'enrollment/section-list.html')
+    section_list = Section.objects.all()
+    context = {
+        'section_list' : section_list
+    }
+    return render(request,'enrollment/section-list.html', context)
--- static/02-javascript/back-end-scripts.js
+++ static/02-javascript/back-end-scripts.js
@@ -9,35 +9,3 @@ function enrollmentTable() {
         }
     });
 }
-$(".js-create-registration").click(function() {
-    $.ajax({
-        url: '{% url "enrollment-create" student.student_ID%}',
-        type: 'get',
-        dataType: 'json',
-        beforeSend: function() {
-            $("#addModal").modal("show");
-        },
-        success: function(data) {
-            $("#addModal .modal-content").html(data.html_form);
-        }
-    });
-});
-
-$("#addModal").on("submit", ".enrollment-form", function() {
-    var form = $(this);
-    $.ajax({
-        url: form.attr("action"),
-        data: form.serialize(),
-        type: form.attr("method"),
-        dataType: 'json',
-        success: function(data) {
-            if (data.form_is_valid) {
-                location.reload()
-            }
-            else {
-                $("#enrollment-modal .modal-content").html(data.html_form);
-            }
-        }
-    });
-    return false;
-});
