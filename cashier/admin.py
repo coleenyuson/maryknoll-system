032171bb6b75b23ac2102bc6245cf94c5b75b0cb
Aji <jwpbanawan@addu.edu.ph>
2018-02-28 06:56:40 +0000

    Fixed log out button and username display on 'Welcome Steve Harrington'

--- cashier/admin.py
+++ cashier/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(FeesAccounts)
-admin.site.register(Payments)
-admin.site.register(PaymentDetails)
+@admin.register(Particular)
+class ParticularAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Particular._meta.get_fields()]
---
+++ cashier/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import *
+
+class ParticularForms(forms.ModelForm):
+    class Meta:
+        model = Particular
+        exclude = ('particular_ID',)
+''' ACCOUNTS FORMS '''
+
+class AccountForms(forms.ModelForm):
+    class Meta:
+        model = Account
+        exclude = ('account_ID',)
+
+class Account_ParticularForms(forms.ModelForm):
+    class Meta:
+        model = Account_Particular
+        exclude = ('account_particular_ID',)
+        
+''' TRANSACTION FORMS '''
+
+class TransactionForms(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        exclude = ('date_time_created',)
+
+class Transaction_DetailForms(forms.ModelForm):
+    class Meta:
+        model = Transaction_Detail
+        exclude = ('transaction_detail_ID','transact_ID',)
+        
+class Daily_CashForms(forms.ModelForm):
+    class Meta:
+        model = Daily_Cash
+        exclude = ('daily_cash_ID',)
