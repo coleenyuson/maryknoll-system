7fe7a7bdc599babfc4cc3743b51c33587ca80939
Aji <jwpbanawan@addu.edu.ph>
2018-02-28 16:53:59 +0000

    Fixed registration/views.py line 166 error on parameters

--- registration/views.py
+++ registration/views.py
@@ -163,7 +163,7 @@ def verifyActive():
       
       
 #CUSTOM MADE FUNCTIONS -------------------------------------------------------------
-def generateStudentCode(Student student_ID):
+def generateStudentCode(student):
     pass
 #REPORTS -------------------------------------------------------------
 
