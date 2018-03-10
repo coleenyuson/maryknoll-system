dc71a0d95c66a58a5688bb9095f73f3ddd5a6a77
Aji <jwpbanawan@addu.edu.ph>
2018-03-11 04:13:02 +0800

    registrar-fixes

--- registration/urls.py
+++ registration/urls.py
@@ -3,12 +3,11 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.index, name = 'reg-index'),
         url(r'^student-list/$', views.studentList, name = 'student-list'),
         url(r'^student-list/download$', views.Export_Model_To_CSV.as_view(), name = 'student-list-download'), #new feature, delete this line if you have seen this already -aji
         url(r'^student-list/table$', views.table_StudentList, name = 'student-table'),
         url(r'^student-list/add$', views.addStudent, name = 'student-add'),
-        url(r'^student-list/create$', views.form_addStudent, name = 'student-add-form'),
+        url(r'^student-list/create$', views.form_addStudent, name = 'student-create'),
         url(r'^student-list/update/(?P<pk>\d+)$', views.updateStudentProfile, name = 'student-update'),
         url(r'^student-list/edit-form/(?P<pk>\d+)$', views.form_updateStudentProfile, name = 'student-edit-form'),
         #student profile urls
